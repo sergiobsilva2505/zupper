07db94b07919b94c2face723bafaa26b0d6e4467
sergiobsilva2505 <sergio.bezerra@outlook.com>
2021-04-15 21:57:15 -0300

    test: rest

--- src/main/java/br/com/sbs/testezup/TestezupApplication.java
+++ src/main/java/br/com/sbs/testezup/TestezupApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestezupApplication {
 
     public static void main(String[] args) {
-
-
         SpringApplication.run(TestezupApplication.class, args);
     }
 
