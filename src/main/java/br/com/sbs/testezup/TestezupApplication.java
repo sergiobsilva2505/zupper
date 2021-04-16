6dad462d928d860d05adf46b00509d59675fd3c9
sergiobsilva2505 <sergio.bezerra@outlook.com>
2021-04-15 21:37:06 -0300

    Init

---
+++ src/main/java/br/com/sbs/testezup/TestezupApplication.java
@@ -0,0 +1,15 @@
+package br.com.sbs.testezup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestezupApplication {
+
+    public static void main(String[] args) {
+
+
+        SpringApplication.run(TestezupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/sbs/testezup/TestezupApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sbs.testezup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestezupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
