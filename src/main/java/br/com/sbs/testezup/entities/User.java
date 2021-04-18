5252652974e7208b85b416163bad59d15f94ccf0
sergiobsilva2505 <sergio.bezerra@outlook.com>
2021-04-18 18:32:31 -0300

    feat(user): tests data

--- src/main/java/br/com/sbs/testezup/entities/User.java
+++ src/main/java/br/com/sbs/testezup/entities/User.java
@@ -1,12 +1,19 @@
 package br.com.sbs.testezup.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+@Entity
 public class User implements Serializable{
 
     private  static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String email;
---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,5 @@
+INSERT INTO USER ( NAME , CPF,  DATE_OF_BIRTH , EMAIL ) VALUES ('Cauê Benedito Araújo', '80563885831', '1989-07-15', 'caue.araujo@gmail.com');
+INSERT INTO USER ( NAME , CPF,  DATE_OF_BIRTH , EMAIL ) VALUES ('Severino Calebe Nogueira', '37868581875', '1982-06-15', 'severino.nogueira@gmail.com');
+INSERT INTO USER ( NAME , CPF,  DATE_OF_BIRTH , EMAIL ) VALUES ('Nicole Aline Sophia Galvão', '57029218876', '1980-06-21', 'nicole.galvao@gmail.com');
+INSERT INTO USER ( NAME , CPF,  DATE_OF_BIRTH , EMAIL ) VALUES ('Ayla Laura Campos', '13225604801', '1976-03-17', 'ayla.campos@gmail.com');
+INSERT INTO USER ( NAME , CPF,  DATE_OF_BIRTH , EMAIL ) VALUES ('Theo Otávio Figueiredo', '61954077807', '1971-11-20', 'theo.figueiredo@gmail.com');
