3f424477e8c04e31539f65844da4f6b1f2c44f2b
sergiobsilva2505 <sergio.bezerra@outlook.com>
2021-04-18 19:00:12 -0300

    feat(user): findByIdController created

--- src/main/java/br/com/sbs/testezup/controllers/UserController.java
+++ src/main/java/br/com/sbs/testezup/controllers/UserController.java
@@ -1,20 +1,28 @@
 package br.com.sbs.testezup.controllers;
 
+import br.com.sbs.testezup.dto.UserDTO;
 import br.com.sbs.testezup.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
 
-    @GetMapping
-    public User teste(){
-        User user =new User(1, "Sergio Bezerra da Silva", "zedahora@gmail.com", "16958158822",
-                LocalDate.of(1974,05,25));
-        return user;
+    @Autowired
+    private UserSerice userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> findUserById(@PathVariable Integer id){
+        Optional<User> obj = userService.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj.get()));
     }
 }
---
+++ src/main/java/br/com/sbs/testezup/dto/UserDTO.java
@@ -0,0 +1,62 @@
+package br.com.sbs.testezup.dto;
+
+import br.com.sbs.testezup.entities.User;
+
+import java.time.LocalDate;
+
+public class UserDTO {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String cpf;
+    private LocalDate dateOfBirth;
+
+    public UserDTO(User user) {
+        this.id = user.getId();
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.cpf = user.getCpf();
+        this.dateOfBirth = user.getDateOfBirth();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
