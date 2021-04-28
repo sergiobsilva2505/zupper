4b4e869100cab434447aa0912bd9bbd5e89d9fdd
sergiobsilva2505 <sergio.bezerra@outlook.com>
2021-04-28 19:50:07 -0300

    feat(viaCep-integration): configuration

--- src/main/java/br/com/sbs/testezup/TestezupApplication.java
+++ src/main/java/br/com/sbs/testezup/TestezupApplication.java
@@ -2,8 +2,10 @@ package br.com.sbs.testezup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class TestezupApplication {
 
     public static void main(String[] args) {
